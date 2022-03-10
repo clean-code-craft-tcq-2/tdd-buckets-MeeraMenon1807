72bf5c8575a485437f31250ca2c9739c91abbb78
MeeraMenon1807 <91048876+MeeraMenon1807@users.noreply.github.com>
2022-03-10 11:35:18 +0530

    Update test-tdd.cpp

--- test-tdd.cpp
+++ test-tdd.cpp
@@ -2,6 +2,7 @@
 
 #include "test/catch.hpp"
 #include "sort.h"
+#include "ranges.h"
 
 SCENARIO("sort an unsorted array")
 {
@@ -22,4 +23,40 @@ SCENARIO("sort an unsorted array")
             }
         }
     }
+}
+
+SCENARIO("get reading ranges and count form input array")
+{
+    GIVEN("an array with set of periodic current samples from a charging session is provided")
+    {
+        int arr[7] = {3, 3, 5, 4, 10, 11, 12};
+        ReadingRange expectedReadingRange[2] = {{3, 5, 4}, {10, 12, 3}};
+        ReadingRangeList expectedReadingRangeList = {expectedReadingRange, 2};
+
+        WHEN("getReadingRangeList() is called with input array and size")
+        {
+            ReadingRangeList actualReadingRangeList = getReadingRangeList(arr, 7);
+            THEN("list with reading ranges and number of readings in each range is returned")
+            {
+                REQUIRE(actualReadingRangeList.totalReadingRanges == expectedReadingRangeList.totalReadingRanges);
+
+                int isEqual = 1;
+                ReadingRange *actualReadingRange = actualReadingRangeList.readingRanges;
+                for (int i = 0; i < expectedReadingRangeList.totalReadingRanges; i++)
+                {
+                    REQUIRE(actualReadingRange[i].rangeStart == expectedReadingRange[i].rangeStart);
+                    REQUIRE(actualReadingRange[i].rangeEnd == expectedReadingRange[i].rangeEnd);
+                    REQUIRE(actualReadingRange[i].numberOfReadings == expectedReadingRange[i].numberOfReadings);
+                }
+            }
+        }
+    }
+}
+
+TEST_CASE("test printReadingRangeList() does not throw error")
+{
+    int arr[7] = {3, 3, 5, 4, 10, 11, 12};
+    ReadingRange readingRange[2] = {{3, 5, 4}, {10, 12, 3}};
+    ReadingRangeList readingRangeList = {readingRange, 2};
+    REQUIRE_NOTHROW(printReadingRangeList(readingRangeList));
 }
